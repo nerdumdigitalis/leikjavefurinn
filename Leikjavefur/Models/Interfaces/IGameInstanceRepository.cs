1c762195b2294d89af045f1a466f43a3b31d9e05
TechhTani <natan_ola@hotmail.com>
2013-05-10 17:25:43 +0000

    Updated chat, now you cannot chat unless you are logged in.. and your name appears when you chat

---
+++ Leikjavefur/Models/Interfaces/IGameInstanceRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Leikjavefur.Models.Interfaces
+{
+    public interface IGameInstanceRepository : IDisposable
+    {
+        IQueryable<GameInstance> All { get; }
+        IQueryable<GameInstance> AllIncluding(params Expression<Func<GameInstance, object>>[] includeProperties);
+    }
+}
---
+++ Leikjavefur/ViewModels/ChatModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Leikjavefur.ViewModels
+{
+    public class ChatModel
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public int gameInstance { get; set; }
+    }
+}
