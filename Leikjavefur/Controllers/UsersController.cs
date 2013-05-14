e276f5ca873bc6ffb273fa31f120ac054542148a
sverrirt11 <sverrirt11@ru.is>
2013-05-14 21:57:16 +0000

    Enable/Disabled Users fyrir admins

--- Leikjavefur/Controllers/UsersController.cs
+++ Leikjavefur/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using Leikjavefur.Models;
 using Leikjavefur.Models.Interfaces;
 using Leikjavefur.Models.Repository;
 using WebMatrix.WebData;
+using System.Web.Security;
 
 namespace Leikjavefur.Controllers
 {   
@@ -136,8 +137,18 @@ namespace Leikjavefur.Controllers
         public ActionResult _ProfilePartial()
         {
             return PartialView(_dataRepository.UserRepository.Find(WebSecurity.CurrentUserId));
+        }
+
+        public ActionResult ToggleDisable(string name)
+        {
+            if (Roles.IsUserInRole(name, "Disabled"))
+                Roles.RemoveUsersFromRoles(new[] { name }, new[] { "Disabled" });
+            else
+                Roles.AddUsersToRoles(new[] { name }, new[] { "Disabled" });
+
+            return RedirectToAction("Index");
         }    
-        
+
     }
 }
 
